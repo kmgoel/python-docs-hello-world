22779cfe3c44af47c8bb2aa07f2a6802894b9125
kmgoel <84813228+kmgoel@users.noreply.github.com>
2021-05-28 13:17:20 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
-from flask import Flask
+#@app.route("/")
+#def hello():
+    #return "Hello From Kims branch!"
+
+
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello From Kims branch!"
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
