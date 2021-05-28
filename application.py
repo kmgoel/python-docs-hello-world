41aa5b5e0fa70a9c98ffc1de8670c32590dd8f11
kmgoel <84813228+kmgoel@users.noreply.github.com>
2021-05-28 14:11:04 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect
-#from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
