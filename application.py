27e4dfb84c642963cb55a8d64135e771e1396b6a
kmgoel <84813228+kmgoel@users.noreply.github.com>
2021-05-28 14:34:56 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, This is Kims Azure Web App using Flask!"
 
-
-from twilio.twiml.messaging_response import MessagingResponse
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     # Respond to incoming calls with a simple text message.
@@ -17,12 +17,8 @@ def sms_reply():
     resp.message("This is a reply from the Azure Web App using Twilio & Flask")
 
     return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-"""
     
-#@app.route("/sms", methods=['GET', 'POST'])
+#@app.route("/sms2", methods=['GET', 'POST'])
 def incoming_sms():
     # Send a dynamic reply to an incoming text message
     # Get the message the user sent our Twilio number
@@ -41,4 +37,4 @@ def incoming_sms():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    """
+    
